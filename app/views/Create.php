13ad2969f3fb9f34e80168cf26f874255fb3c7f9
reyilleelize0807 <patricioreyilleelize@gmail.com>
2025-09-15 15:55:36 +0800

    First commit

--- app/views/Create.php
+++ app/views/Create.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create</title>
-     <link rel="stylesheet" href="<?=base_url();?>public/css/style3.css">
+    <link rel="stylesheet" href="<?=base_url();?>public/css/style3.css">
 </head>
 <body>
     <div class="container">
