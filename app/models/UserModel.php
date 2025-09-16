ee5e0be420073b4dbdb479496df7e3605b97b294
reyilleelize0807 <patricioreyilleelize@gmail.com>
2025-09-16 23:40:11 +0800

    lastnadapat

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -7,12 +7,29 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
  * Automatically generated via CLI.
  */
 class UserModel extends Model {
-    protected $table = 'students';
+    protected $table = 'users';
     protected $primary_key = 'id';
 
     public function __construct()
     {
         parent::__construct();
+        $this->ensure_table_exists();
+    }
+    
+    private function ensure_table_exists()
+    {
+        try {
+            $this->db->raw("CREATE TABLE IF NOT EXISTS `users` (
+                `id` INT(11) NOT NULL AUTO_INCREMENT,
+                `last_name` VARCHAR(100) NOT NULL,
+                `first_name` VARCHAR(100) NOT NULL,
+                `email` VARCHAR(255) NOT NULL,
+                `deleted_at` DATETIME NULL DEFAULT NULL,
+                PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
+        } catch (Exception $e) {
+            // Silently ignore to avoid breaking requests if DB user lacks DDL permissions
+        }
     }
     
 }
